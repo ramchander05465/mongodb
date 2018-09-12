98bb849faedb53d02410c1e15d69c62bcfa48d9d
Ram.Chander <cram@netapp.com>
2018-09-12 14:28:24 +0530

    modify helper class for opening mongodb connection

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
-
-mongoose.connect('mongodb://localhost/MongodbPOC');
-mongoose.connection
-    .once('open', () => console.log('good to go'))
+before((done)=>{
+    mongoose.connect('mongodb://localhost/MongodbPOC');
+    mongoose.connection
+    .once('open', () => done())
     .on('error', (error) => {
         console.warn('warning', error)
     })
+})
+
 
 beforeEach((done) => {
     mongoose.connection.collections.logins.drop(()=>{
